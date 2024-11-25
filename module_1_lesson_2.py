90ffd7e8ef3453cc58ebaf2c84f4528d4110b74e
Daria <step.dasha.2000@gmail.com>
2024-11-25 23:59:17 +0300

    Update and rename module_1_lesson_2.py to module_1_2.py

--- module_1_lesson_2.py
+++
@@ -1,5 +0,0 @@
-homework_count = 12
-hours_count = 1.5
-course_name = 'Python'
-one_task_time = 0.125
-print('Курс:', course_name,',' 'всего задач:', homework_count,',' 'затрачено часов:', hours_count,',' 'среднее время выполнения', one_task_time, 'часа.')
